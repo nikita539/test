2158230abccdc2f42a9d6522234a0b8bd56ac45b
Nikita <evteev_n@inbox.ru>
2024-06-10 20:33:11 +0300

    random changes

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-function count(number) {
-    for (let i = 0; i <= 10; i++) {
-        console.log(i)
-    }
+let count = 0;
+
+for (let i = 0; i < 10; i++) {
+    count = i
 }
+
+
 function recursionSum(count) {
-    if (count == 0) return 0
+    if (count == 0) return 1
     return count + recursionSum(count - 1)
 } 
 
+const answer = recursionSum(10)
 
+console.log(answer)
