67b01ff5ccc1086bbc0f9e827f9aae3aeea108d8
Nikita <evteev_n@inbox.ru>
2024-07-06 20:21:55 +0300

    some changes

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-const mainTitle = document.getElementById('title')
-const counter = document.getElementById('counter')
-const countValue = document.getElementById('count')
-
-counter.addEventListener('click', function() {
-    countValue.innerText = +countValue.innerText + 1
-    console.log(typeof +countValue.innerText)
-    count = count + 1
-})
+console.log("Hello World !")
