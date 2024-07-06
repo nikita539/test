7321d9c0ed75c7a4820cf3be8053c1e264137245
Nikita <evteev_n@inbox.ru>
2024-07-06 19:38:10 +0300

    some useless code

--- helpers.js
+++ helpers.js
@@ -1,3 +1,5 @@
 function findProperty (path, obj) {  // finds property by path of format: "user.brother.name"
-    
+    Object.keys(obj).reduce((prev,curr) => {
+        return prev[curr]
+    }, obj)
 }
