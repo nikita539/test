03fc750a9231a13e6cef99b866035eda3f914b27
Nikita <evteev_n@inbox.ru>
2024-07-07 20:47:23 +0300

    css refactoring

--- helpers.js
+++ helpers.js
@@ -1,5 +1,5 @@
 function findProperty (path, obj) {  // finds property by path of format: "user.brother.name"
-    Object.keys(obj).reduce((prev,curr) => {
+    return path.split('.').reduce((prev,curr) => {
         return prev[curr]
-    }, obj)
+    }, obj) || null
 }
