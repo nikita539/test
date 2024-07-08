b13144650ffdd04955a162241f7defcb6c94aba7
Nikita <evteev_n@inbox.ru>
2024-07-08 16:31:30 +0300

    iplemented drop-back function

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const plants = document.getElementsByClassName('plant')
+const plantsPlaces = document.getElementsByClassName('plant_container')
 const dropzone = document.getElementById('dropezone')
 
 for (let i = 0; i < plants.length; i++) {
     plants[i].addEventListener('dragstart', function (event) {
         event.dataTransfer.setData('text/plain', event.target.id)
     })
+}
+
+for (let i = 0; i < plantsPlaces.length; i++) {
+    plantsPlaces[i].ondrop = function(event) {
+        event.preventDefault()
+
+        const elem = document.getElementById(event.dataTransfer.getData('text/plain'))
+        event.target.appendChild(elem)
+    }
+
+    plantsPlaces[i].ondragover = function(event) {
+        event.preventDefault()
+    }
 }
 
 dropzone.ondrop = function(event) {
@@ -16,4 +30,5 @@ dropzone.ondrop = function(event) {
 
 dropzone.ondragover = function(event) {
     event.preventDefault()
+    console.log('dropzoneondragover')
 }
