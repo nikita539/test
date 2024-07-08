5def0d8bedf8392306137d7ff44b5952f5233407
Nikita <evteev_n@inbox.ru>
2024-07-08 11:30:35 +0300

    implemented drag and drop functionality

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const dropzone = document.getElementById('dropezone')
 
 for (let i = 0; i < plants.length; i++) {
     plants[i].addEventListener('dragstart', function (event) {
-        event.dataTransfer.setData('text/html', event.target.outerHTML)
+        event.dataTransfer.setData('text/plain', event.target.id)
     })
 }
 
 dropzone.ondrop = function(event) {
     event.preventDefault()
     event.dataTransfer.dropEffect = "move"
+    const elem = document.getElementById(event.dataTransfer.getData('text/plain'))
+    event.target.appendChild(elem)
 }
+
 dropzone.ondragover = function(event) {
     event.preventDefault()
 }
