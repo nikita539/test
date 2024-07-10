4845901950d81eb59090f3867b7ae01cfb16430b
Nikita <evteev_n@inbox.ru>
2024-07-10 23:29:39 +0300

    started to implement drag and drop functionality by another way

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
 const plants = document.getElementsByClassName('plant')
-const plantsPlaces = document.getElementsByClassName('plant_container')
-const dropzone = document.getElementById('dropezone')
 
-for (let i = 0; i < plants.length; i++) {
-    plants[i].addEventListener('dragstart', function (event) {
-        event.dataTransfer.setData('text/plain', event.target.id)
-    })
+for (let i = 0; i <= plants.length; i++) {
+    dragElement(plants[i])
 }
 
-for (let i = 0; i < plantsPlaces.length; i++) {
-    plantsPlaces[i].ondrop = function(event) {
-        event.preventDefault()
 
-        const elem = document.getElementById(event.dataTransfer.getData('text/plain'))
-        event.target.appendChild(elem)
-    }
-
-    plantsPlaces[i].ondragover = function(event) {
-        event.preventDefault()
+function dragElement(element) {
+    element.onclick = function(event) {
+        console.log('Hello World !')
     }
 }
-
-dropzone.addEventListener('drop', function(event) {
-    event.preventDefault()
-    event.dataTransfer.dropEffect = "move"
-    const elem = document.getElementById(event.dataTransfer.getData('text/plain'))
-    event.target.appendChild(elem)
-})
-
-dropzone.addEventListener('dragover', function(event) {
-    event.preventDefault()
-})
