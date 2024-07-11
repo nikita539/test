a06b06ae950cef1623bb7e315c748f558c90d247
Nikita <evteev_n@inbox.ru>
2024-07-11 22:30:00 +0300

    deleted comments

--- index.js
+++ index.js
@@ -1,48 +1,40 @@
 const plants = document.getElementsByClassName('plant')
 
-for (let i = 0; i <= plants.length; i++) {
+for (let i = 0; i < plants.length; i++) {
     dragElement(plants[i])
 }
 
 
 function dragElement(terrariumElement) {
-	//set 4 positions for positioning on the screen
 	let pos1 = 0,
 		pos2 = 0,
 		pos3 = 0,
 		pos4 = 0;
-	terrariumElement.onpointerdown = pointerDrag;
+    
+	terrariumElement.onpointerdown = pointerDrag
+    
 
 	function pointerDrag(e) {
 		e.preventDefault();
-		console.log(e);
-		// get the initial mouse cursor position for pos3 and pos4
+
 		pos3 = e.clientX;
 		pos4 = e.clientY;
-		// when the mouse moves, start the drag
+
 		document.onpointermove = elementDrag;
-		// when the mouse is lifted, stop the drag
 		document.onpointerup = stopElementDrag;
 	}
 
 	function elementDrag(e) {
-		// calculate the new cursor position
-		// pos1 = where the Xmouse WAS - where it IS
 		pos1 = pos3 - e.clientX;
-		// pos2 = where the Ymouse WAS - where it IS
 		pos2 = pos4 - e.clientY;
-		//reset pos3 to current location of Xmouse
 		pos3 = e.clientX;
-		//reset pos4 to current location of Ymouse
 		pos4 = e.clientY;
-		console.log(pos1, pos2, pos3, pos4);
-		// set the element's new position:
+
 		terrariumElement.style.top = terrariumElement.offsetTop - pos2 + 'px';
 		terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + 'px';
 	}
 
 	function stopElementDrag() {
-		// stop calculating when mouse is released
 		document.onpointerup = null;
 		document.onpointermove = null;
 	}
